379899e3afa76ad6317fee361f38b2ad16444bee
Miles Young <miles@cal-solar.com>
2024-02-05 08:59:39 -0800

    initial commit

---
+++ solaredge/app.js
@@ -0,0 +1,89 @@
+const axios = require('axios')
+
+const API_ACCOUNT_TOKEN = '7BUPHYY5GQMTW9HR7MQA8RUK3LG3RAUA'
+
+const SITE_ID = '1951350'
+const PTO_DATE = '2020-12-21'
+const PRODUCTION_TOTALS = {
+    1: 9451,
+    2: null,
+    3: 0
+}
+
+
+const verifyCurrentProductionYear = (ptoDate, currentProductionTotals) => {
+    const nullIndexArray = []
+    const zeroIndexArray = []
+    for(const [key, value] of Object.entries(currentProductionTotals)) {
+        if(value == null) {
+            nullIndexArray.push(key)
+        } else if(value === 0) {
+            zeroIndexArray.push(key)
+        }
+    }
+
+    if((nullIndexArray.length===0) && (zeroIndexArray.length===0)) {
+        console.log('all production years are filled???')
+    }
+
+    const fullYearsObject = getFullYearsSincePTO(ptoDate)
+    if(fullYearsObject == null) {
+        console.log('it has not yet been a full year')
+    }
+
+    const yearNumber = Number(nullIndexArray[0])
+    // have to subtract 1 because the returnYears from getFullYearsSincePTO returns a zero indexed array (year 1 is at position 0)
+    yearStartAndEndArray = fullYearsObject[yearNumber-1]
+
+    console.log(yearStartAndEndArray)
+
+
+    return {nullIndexArray, zeroIndexArray}
+}
+
+const getFullYearsSincePTO = (ptoDate) => {
+    const returnYears = []
+    let i = 1
+    let startingYear = new Date(ptoDate)
+    while(i < 4) {
+        const yearEndDate = getAYearFromDate(startingYear)
+        if(yearEndDate == null) {
+            console.log('we have a null')
+            break
+        }
+        returnYears.push([startingYear.toISOString().split('T')[0], yearEndDate.toISOString().split('T')[0]])
+        startingYear = yearEndDate
+
+        i++
+    }
+    if(returnYears.length === 0) {
+        return null
+    }
+    return returnYears
+}
+// if a year from "date" is in future return null
+const getAYearFromDate = (date) => {
+    const currentDate = new Date()
+    let yearAwayDate = new Date(date)
+    yearAwayDate.setFullYear(yearAwayDate.getFullYear() + 1)
+    if(yearAwayDate > currentDate) {
+        return null
+    }
+    return yearAwayDate
+}
+
+verifyCurrentProductionYear(PTO_DATE, PRODUCTION_TOTALS)
+
+
+
+
+//return {value:year production, nullCount: count of null dates}
+const getYearData = (siteId, ptoDate, ) => {
+    const endDate = getEndDate(startDate)
+    const MAIN_REQUEST_URL = `https://monitoringapi.solaredge.com/site/${siteId}/energy?timeUnit=DAY&endDate=${endDate}&startDate=${startDate}&api_key=${API_ACCOUNT_TOKEN}`
+}
+
+
+
+
+
---
+++ solaredge/test.js
@@ -0,0 +1,38 @@
+const axios = require('axios')
+
+const API_ACCOUNT_TOKEN = '7BUPHYY5GQMTW9HR7MQA8RUK3LG3RAUA'
+siteId = '1671510'
+startDate = '2022-10-16'
+endDate = '2022-10-07'
+
+const getEndDate = (startDate) => {
+    const date1 = new Date(startDate)
+    const date2 = new Date(endDate)
+
+    console.log(date1.getDate())
+}
+
+getEndDate(startDate)
+
+const requestURL = `https://monitoringapi.solaredge.com/site/${siteId}/energy?timeUnit=DAY&endDate=${endDate}&startDate=${startDate}&api_key=${API_ACCOUNT_TOKEN}`
+
+
+// get a site energy
+// axios.get(requestURL)
+//     .then(res => console.log(res.data.energy.values))
+//     .catch(err => console.log(err))
+
+
+
+// get all sites
+// axios.get(`https://monitoringapi.solaredge.com/sites/list?size=20&&sortProperty=name&sortOrder=ASC&api_key=${API_ACCOUNT_TOKEN}`)
+//     .then(res => console.log(res.data.sites))
+//     .catch(err => console.log(err))
+
+
+
+
+
+
+
+
